9918de4f03c04f46123a5c377fcaeec378a3053e
ptrshkina <ptrshkina@gmail.com>
2022-12-14 00:47:14 +0300

    Tasks 9-10 done
    9. Remove ejections
    10. Check distribution on normality with different criterias

--- README.md
+++ README.md
@@ -4,16 +4,20 @@
 
 [Pandas docs](https://pandas.pydata.org/docs/user_guide/index.html)
 
+[Index in groupping issue](https://stackoverflow.com/questions/19202093/how-to-select-columns-from-groupby-object-in-pandas)
+
 # Add libraries
 
 ```sh
 # Windows
 pip install matplotlib
 pip install pandas
+pip install scipy
 
 # Mac OS
 pip3 install matplotlib
 pip3 install pandas
+pip3 install scipy
 ```
 
 # Run application
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Libraries
 import pandas as pd
 import numpy as np
+import scipy as sc
 import math
 
 
@@ -9,10 +10,24 @@ def checkValidity():  # Check if there are any null values in dataset
     print(f'Data is {"valid" if isValid else "invalid"}')
     return isValid
 
+# Helpers
+
+
+class bcolors:
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+null = np.nan
 
 # 5. Load dataframe
 dataFrame = pd.read_csv('./marketing.csv')
-null = np.nan
 
 # 6. Build plots
 dataFrame.groupby('AgeOfStore').median(numeric_only=True).plot.bar(
@@ -36,7 +51,7 @@ if (not checkValidity()):
     for i, row in enumerate(dataFrame.values):
         # If marketSize is empty - set from any non empty entry with current marketId
         if (row[1] is null):
-            print('Empty \033[94mMARKET_SIZE\033[0m found:', row)
+            print(f'Empty {bcolors.YELLOW}MARKET_SIZE{bcolors.END} found:', row)
             notEmpty = dataFrame.loc[(dataFrame['MarketID'] == row[0])
                                      & (dataFrame['MarketSize'] != null)].values[0]
             # Set current MarketSize to notEmpty's market size
@@ -44,7 +59,8 @@ if (not checkValidity()):
             print(f'    Filled with "{dataFrame.values[i][1]}"')
         # If AgeOfStore is empty
         if (math.isnan(row[3])):
-            print('Empty \033[92mAGE_OF_STORE\033[0m found:', row)
+            print(
+                f'Empty {bcolors.BLUE}AGE_OF_STORE{bcolors.END} found:', row)
             notEmpty = dataFrame.loc[(
                 dataFrame['LocationID'] == row[2]) & (dataFrame['AgeOfStore'].notnull())].values[0]
             print(notEmpty)
@@ -52,7 +68,8 @@ if (not checkValidity()):
             print(f'    Filled with "{dataFrame.values[i][3]}"')
         # If SalesInThousands is empty
         if (math.isnan(row[6])):
-            print(f'Empty \033[91mSALES_IN_THOUSANDS\033[0m found', row)
+            print(
+                f'Empty {bcolors.RED}SALES_IN_THOUSANDS{bcolors.END} found', row)
             rowsToDrop.append(i)
             print(f'    Row "{i}" marked as deleteable')
     # Drop rows, marked as deleteable
@@ -60,4 +77,34 @@ if (not checkValidity()):
 # Check validity again to confirm
 checkValidity()
 
-# 9: 
+# 9: Remove ejections of small market by left trust interval
+smallMarketDataFrame = dataFrame.loc[dataFrame['MarketSize'] == 'Small']
+# Make distribution plot
+smallMarketDataFrame[['SalesInThousands']].plot.hist(
+    bins=12, alpha=0.5, density=True, edgecolor='w', linewidth=0.5).get_figure().savefig('distribution', dpi=300)
+filteredDataFrame = smallMarketDataFrame.loc[dataFrame['SalesInThousands'] > 40]
+print(filteredDataFrame)
+print(f'Ejections removed:',
+      smallMarketDataFrame.loc[dataFrame['SalesInThousands'] < 40].values)
+
+# 10: Check if normal distribute
+# Visual method
+smallMarketDataFrame[['SalesInThousands']].plot.density(
+).get_figure().savefig('distribution_normal', dpi=300)
+alpha = 0.05
+# Shapiro-Wilk criteria
+stat, p = sc.stats.shapiro(smallMarketDataFrame['SalesInThousands'])
+print(f'[Shapiro-Wilk] Alpha={alpha} Statistics={stat}, p-value={p}')
+print(f'    Distribution is',
+      f"{bcolors.GREEN}normal{bcolors.END}" if p > alpha else f"{bcolors.RED}abnormal{bcolors.END}")
+# Pearson criteria
+stat, p = sc.stats.normaltest(smallMarketDataFrame['SalesInThousands'])
+print(f'[Pearson] Alpha={alpha} Statistics={stat}, p-value={p}')
+print(f'    Distribution is',
+      f"{bcolors.GREEN}normal{bcolors.END}" if p > alpha else f"{bcolors.RED}abnormal{bcolors.END}")
+
+# 11:
+
+# 12:
+
+# 13:
